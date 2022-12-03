1597379a26ba28773078f401d003232095b62e5f
Jeff <cyjeffb@yahoo.com>
2022-12-03 14:03:18 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Udemy: Java Programming Masterclass");
+        System.out.println("Coding Challenge: Minimum and Maximum Challenge");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int minimum = 0;
+        int maximum = 0;
+        while(true){
+            System.out.println("Enter number: ");
+            boolean hasAnInt = scanner.hasNextInt();
+            if(hasAnInt){
+                int number = scanner.nextInt();
+                if(minimum == 0 ) {
+                    minimum = number;
+                } else if(minimum > number) {
+                    minimum = number;
+                } else if(minimum < number){
+                    maximum = number;
+                }
+            } else{
+                break;
+            }
+        }
+
+        System.out.println("Minimum number: " + minimum + ", Maximum number: " + maximum);
+        scanner.close();
     }
 }
