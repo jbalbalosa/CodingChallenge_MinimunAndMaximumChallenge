80df0adf506d251361547a4de62d376d93477500
Jeff <cyjeffb@yahoo.com>
2022-12-03 15:05:03 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Udemy: Java Programming Masterclass");
         System.out.println("Coding Challenge: Minimum and Maximum Challenge");
+        System.out.println("");
 
         Scanner scanner = new Scanner(System.in);
 
         int minimum = 0;
         int maximum = 0;
         while(true){
-            System.out.println("Enter number: ");
+            System.out.print("Enter number: ");
             boolean hasAnInt = scanner.hasNextInt();
             if(hasAnInt){
                 int number = scanner.nextInt();
-                if(minimum == 0 ) {
+                if(minimum == 0 && maximum == 0) {
                     minimum = number;
-                } else if(minimum > number) {
+                    maximum = number;
+                } else if(number < minimum) {
                     minimum = number;
-                } else if(minimum < number){
+                } else if(number > maximum){
                     maximum = number;
                 }
+
             } else{
                 break;
             }
